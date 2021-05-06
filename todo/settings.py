1705823d4cf38ad16b4bcd20e7df8ddcc634c62c
alanrr33 <alanrr33@gmail.com>
2021-05-06 17:35:04 -0300

    Arreglando redireccionamentio en base templata

--- todo/settings.py
+++ todo/settings.py
@@ -132,3 +132,8 @@ LOGIN_URL='/login'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("No se encontro el archivo local_settings")
